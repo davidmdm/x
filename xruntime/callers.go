56e3bb96fb5018e2a29deaa1a157aca2b467eaab
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-06-10 09:27:06 -0400

    xruntime: GetCallStack default to max depth of 100

--- xruntime/callers.go
+++ xruntime/callers.go
@@ -19,8 +19,12 @@ func (stack Stack) String() string {
 }
 
 // GetCallStack returns the stack starting from where GetCallStack and going back as deep as specified by depth.
-// The xruntime.Stack value satisfies the stringer interface for ease of use in common programs..
+// The xruntime.Stack value satisfies the stringer interface for ease of use in common programs.
+// If depth is less than 1 it defaults to a max depth of 100.
 func GetCallStack(depth int) (stack Stack) {
+	if depth < 1 {
+		depth = 100
+	}
 	pc := make([]uintptr, depth)
 
 	max := runtime.Callers(2, pc)
